64f4784d26c116052064c9e195f74a6d7866b345
Demchenko Vyacheslav <frontend-ninja@yandex.ru>
2020-10-09 12:06:10 +0300

    Finished cards js desktop

--- js/main.js
+++ js/main.js
@@ -15,16 +15,214 @@ cardClose.addEventListener('click', function () {
   overlay.classList.remove('overlay--active');
   body.classList.remove('no-scroll');
 });
+
+function backToTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -80);
+    setTimeout(backToTop, 0);
+  }
+}
+
+cardDetails.forEach(function (btn) {
+  btn.addEventListener('click', backToTop);
+});
+var toys = {
+  shark: {
+    name: 'Акула',
+    size: '80 см',
+    material: 'плюш',
+    wadding: 'холлофайбер',
+    descr: 'Мягкая игрушка акула, не смотря на грозный вид, станет отличным другом для вашей семьи',
+    alt: 'Игрушка акула',
+    price: '1200 ₽',
+    img_1_sm: 'img/card-shark-sm@2x.jpg',
+    img_2_sm: 'img/card-shark-sm-2@2x.jpg',
+    img_3_sm: 'img/card-shark-sm-3@2x.jpg',
+    img_1_big: 'img/card-shark-big@2x.jpg',
+    img_2_big: 'img/card-shark-big-2@2x.jpg',
+    img_3_big: 'img/card-shark-big-3@2x.jpg',
+    img_1_full: 'img/card-shark-full@2x.jpg',
+    img_2_full: 'img/card-shark-full-2@2x.jpg',
+    img_3_full: 'img/card-shark-full-3@2x.jpg'
+  },
+  avocado: {
+    name: 'Авокадо',
+    size: '35 см',
+    material: 'плюш',
+    wadding: 'холлофайбер',
+    descr: 'Милый и добрый авокадо, станет отличным подаком для ваших детей, второй половинки и всех всех всех',
+    alt: 'Игрушка авокадо',
+    price: '850 ₽',
+    img_1_sm: 'img/card-avocado-sm@2x.jpg',
+    img_2_sm: 'img/card-avocado-sm-2@2x.jpg',
+    img_3_sm: 'img/card-avocado-sm-3@2x.jpg',
+    img_1_big: 'img/card-avocado-big@2x.jpg',
+    img_2_big: 'img/card-avocado-big-2@2x.jpg',
+    img_3_big: 'img/card-avocado-big-3@2x.jpg',
+    img_1_full: 'img/card-avocado-full@2x.jpg',
+    img_2_full: 'img/card-avocado-full-2@2x.jpg',
+    img_3_full: 'img/card-avocado-full-3@2x.jpg'
+  },
+  corgi: {
+    name: 'Корги',
+    size: '35 см',
+    material: 'плюш',
+    wadding: 'холлофайбер',
+    descr: 'Настоящие королевские корги, особая порода собак, требующая особого внимания и большой любви',
+    alt: 'Игрушка корги',
+    price: '850 ₽',
+    img_1_sm: 'img/card-corgi-sm@2x.jpg',
+    img_2_sm: 'img/card-corgi-sm-2@2x.jpg',
+    img_3_sm: 'img/card-corgi-sm-3@2x.jpg',
+    img_1_big: 'img/card-corgi-big@2x.jpg',
+    img_2_big: 'img/card-corgi-big-2@2x.jpg',
+    img_3_big: 'img/card-corgi-big-3@2x.jpg',
+    img_1_full: 'img/card-corgi-full@2x.jpg',
+    img_2_full: 'img/card-corgi-full-2@2x.jpg',
+    img_3_full: 'img/card-corgi-full-3@2x.jpg'
+  },
+  cat: {
+    name: 'Плюшевый кот',
+    size: '120 см',
+    material: 'плюш',
+    wadding: 'холлофайбер',
+    descr: 'Мягкая игрушка кот, как настоящий, только больше, мягче и дешевле',
+    alt: 'Игрушка плюшевый кот',
+    price: '1900 ₽',
+    img_1_sm: 'img/card-cat-sm@2x.jpg',
+    img_2_sm: 'img/card-cat-sm-2@2x.jpg',
+    img_3_sm: 'img/card-cat-sm-3@2x.jpg',
+    img_1_big: 'img/card-cat-big@2x.jpg',
+    img_2_big: 'img/card-cat-big-2@2x.jpg',
+    img_3_big: 'img/card-cat-big-3@2x.jpg',
+    img_1_full: 'img/card-cat-full@2x.jpg',
+    img_2_full: 'img/card-cat-full-2@2x.jpg',
+    img_3_full: 'img/card-cat-full-3@2x.jpg'
+  },
+  lama: {
+    name: 'Радужная лама',
+    size: '35 см',
+    material: 'плюш',
+    wadding: 'холлофайбер',
+    descr: 'Единственная в своем роде радужная лама, большая удача встретить ее в наше время, но у вас есть шанс ее приручить и стать ее хозяином',
+    alt: 'Игрушка радужная лама',
+    price: '1000 ₽',
+    img_1_sm: 'img/card-lama-sm@2x.jpg',
+    img_2_sm: 'img/card-lama-sm-2@2x.jpg',
+    img_1_big: 'img/card-lama-big@2x.jpg',
+    img_2_big: 'img/card-lama-big-2@2x.jpg',
+    img_1_full: 'img/card-lama-full@2x.jpg',
+    img_2_full: 'img/card-lama-full-2@2x.jpg'
+  },
+  seal: {
+    name: 'Белый тюлень',
+    size: '60 см',
+    material: 'плюш',
+    wadding: 'холлофайбер',
+    descr: 'Белый плюшевый тюлень, редкость для нашей местности, встречается только в нашем магазине',
+    alt: 'Игрушка белый тюлень',
+    price: '1200 ₽',
+    img_1_sm: 'img/card-seal-sm@2x.jpg',
+    img_2_sm: 'img/card-seal-sm-2@2x.jpg',
+    img_1_big: 'img/card-seal-big@2x.jpg',
+    img_2_big: 'img/card-seal-big-2@2x.jpg',
+    img_1_full: 'img/card-seal-full@2x.jpg',
+    img_2_full: 'img/card-seal-full-2@2x.jpg'
+  }
+};
+var attr;
+var currentToy;
+var images = document.querySelectorAll('.img__item img');
+cardDetails.forEach(function (btn) {
+  btn.addEventListener('click', function () {
+    var attr = btn.getAttribute('data-card'); // console.log(attr);
+
+    findToy(attr);
+  });
+});
+
+function findToy(attr) {
+  currentToy = toys[attr]; // console.log(currentToy);
+  // console.log(currentToy.img_1_sm);
+
+  dataFill();
+}
+
+function dataFill() {
+  document.querySelector('.toy-title').textContent = currentToy.name;
+  document.querySelector('.size__num').textContent = currentToy.size;
+  document.querySelector('.material__type').textContent = currentToy.material;
+  document.querySelector('.wadding__type').textContent = currentToy.wadding;
+  document.querySelector('.toy-description').textContent = currentToy.descr;
+  document.querySelector('.price').textContent = currentToy.price;
+  document.querySelector('.img__item-first img').setAttribute('src', currentToy.img_1_sm);
+  document.querySelector('.img__item-second img').setAttribute('src', currentToy.img_2_sm);
+  document.querySelector('.img__item-third img').setAttribute('src', currentToy.img_3_sm);
+  document.querySelector('.img__item-big img').setAttribute('src', currentToy.img_1_big);
+  document.querySelector('.img__item-first img').setAttribute('alt', currentToy.alt);
+  document.querySelector('.img__item-second img').setAttribute('alt', currentToy.alt);
+  document.querySelector('.img__item-third img').setAttribute('alt', currentToy.alt);
+  document.querySelector('.img__item-big img').setAttribute('alt', currentToy.alt);
+  images.forEach(function (img, index) {
+    img.addEventListener('mouseenter', function () {
+      var link = img.getAttribute('src'); // console.log(img);
+
+      var newnewLink = link.replace(/sm/i, 'big');
+      console.log(link);
+      console.log(newnewLink);
+      document.querySelector('.img__item-big img').setAttribute('src', newnewLink);
+    });
+  });
+} // function changeImg() {
+//     images.forEach( (img, index) => {
+//         img.addEventListener('mouseenter', () => {
+//             console.log(img);
+//             console.log(index);
+//             console.log(currentToy);
+//             document.querySelector('.img__item-big img').setAttribute('src', 'currentToy.img_' + (index + 1) + '_full');
+//         });
+//     });
+// }
 "use strict";
 
 var url = document.location.href;
 $.each($('.menu__link'), function () {
   if (this.href == url) {
     $(this).addClass('menu__link--active');
+  } else {
+    $('.menu__link').removeClass('menu__link--active');
   }
-
-  ;
 });
+// if (currentToy == 'lama' || currentToy == 'seal') {
+//     document.querySelector('.toy-title').textContent = currentToy.name;
+//     document.querySelector('.size__num').textContent = currentToy.size;
+//     document.querySelector('.material__type').textContent = currentToy.material;
+//     document.querySelector('.wadding__type').textContent = currentToy.wadding;
+//     document.querySelector('.toy-description').textContent = currentToy.descr;
+//     document.querySelector('.price').textContent = currentToy.price;
+//     document.querySelector('.img__item-first img').setAttribute('src', currentToy.img_1_sm);
+//     document.querySelector('.img__item-second img').setAttribute('src', currentToy.img_2_sm);
+//     document.querySelector('.img__item-big img').setAttribute('src', currentToy.img_1_big);
+//     document.querySelector('.img__item-first img').setAttribute('alt', currentToy.alt);
+//     document.querySelector('.img__item-second img').setAttribute('alt', currentToy.alt);
+//     document.querySelector('.img__item-big img').setAttribute('alt', currentToy.alt);
+// } else {
+//     document.querySelector('.toy-title').textContent = currentToy.name;
+//     document.querySelector('.size__num').textContent = currentToy.size;
+//     document.querySelector('.material__type').textContent = currentToy.material;
+//     document.querySelector('.wadding__type').textContent = currentToy.wadding;
+//     document.querySelector('.toy-description').textContent = currentToy.descr;
+//     document.querySelector('.price').textContent = currentToy.price;
+//     document.querySelector('.img__item-first img').setAttribute('src', currentToy.img_1_sm);
+//     document.querySelector('.img__item-second img').setAttribute('src', currentToy.img_2_sm);
+//     document.querySelector('.img__item-third img').setAttribute('src', currentToy.img_3_sm);
+//     document.querySelector('.img__item-big img').setAttribute('src', currentToy.img_1_big);
+//     document.querySelector('.img__item-first img').setAttribute('alt', currentToy.alt);
+//     document.querySelector('.img__item-second img').setAttribute('alt', currentToy.alt);
+//     document.querySelector('.img__item-third img').setAttribute('alt', currentToy.alt);
+//     document.querySelector('.img__item-big img').setAttribute('alt', currentToy.alt);
+// }
+"use strict";
 "use strict";
 
 /*!
